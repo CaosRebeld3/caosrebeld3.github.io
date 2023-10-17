3510878e871d4c5a60e7167e707d5a679c41006c
YipiReb3l <148220317+YipiReb3l@users.noreply.github.com>
2023-10-17 19:14:39 +0200

    App

---
+++ js/cambiarImagenes.js
@@ -0,0 +1,51 @@
+// created by Yipi Reb3l
+
+//imagenes
+
+ 
+
+    var imagenes=new Array(
+
+        ['img/one.png'],
+
+        ['img/two.png'],
+
+       
+    );
+
+    var contador=0;
+
+
+    function rotarImagenes()
+
+    {
+     
+      contador++
+
+      document.getElementById("mages").src=imagenes[contador%imagenes.length][0];
+      
+
+    }
+
+
+    onload=function()
+
+    {
+
+        rotarImagenes();
+
+        setInterval(rotarImagenes,5000);
+
+    }
+
+
+
+ 
+
+
+ 
+
+
+
+ 
+
