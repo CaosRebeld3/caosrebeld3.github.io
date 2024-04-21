4030e56b31ae511b2b0b3c8f517509480edbb79d
CaosRebeld3 <148220317+CaosRebeld3@users.noreply.github.com>
2024-04-21 18:54:47 +0200

    Add files via upload

---
+++ js/cambiarImagenes.js
@@ -0,0 +1,52 @@
+// Created by CaosRebeld3
+
+//imagenes
+
+ 
+
+    var imagenes=new Array(
+
+
+        ['img/mi.png'],
+        
+        ['img/yo.png'],
+        
+
+      );
+
+    var contador=0;
+
+
+    function rotarImagenes()
+
+    {
+     
+      contador++
+
+      document.getElementById("mages").src=imagenes[contador%imagenes.length][0];
+      
+
+    }
+
+
+    onload=function()
+
+    {
+
+        rotarImagenes();
+
+        setInterval(rotarImagenes,7000);
+
+    }
+
+
+
+ 
+
+
+ 
+
+
+
+ 
+
